0e5f7861952a81fe0429bd0a9d1256b2cb3e6e5f
GomidesTs <gomidestulio5@gmail.com>
2022-10-08 20:27:53 -0300

    :lipstick: feat: standardization of colors used

---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+export default {
+    COLORS: {
+        PRIMARY: '#AC001F',
+        SECONDARY: 'hsla(349, 100%, 34%, 0.2)',
+
+        BLACK_1: '#0B0A0A',
+        BLACK_2: '#191616',
+        BLACK_3: '#262529',
+
+
+        WHITE: '#F4EDE8',
+
+        GRAY_1: '#3E3B47',
+        GRAY_2: '#999591',
+        GRAY_3: '#948F99',
+        GRAY_4: '#CAC4CF',
+        GRAY_5: '#E5E5E5',
+    }
+}

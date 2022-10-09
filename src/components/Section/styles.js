17eaf50801d2751e90782fa4e4f3a6763ec04d0f
GomidesTs <gomidestulio5@gmail.com>
2022-10-08 22:22:03 -0300

    :lipstick: feat: section component styling

---
+++ src/components/Section/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+
+    > h2{
+        width: 100%;
+
+        font-weight: 400;
+        font-size:3.2rem;
+    }
+
+    > Button {
+        max-width:20.7rem;
+    }
+`
